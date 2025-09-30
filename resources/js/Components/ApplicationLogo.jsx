216aa621b8cd0a1c80237e9e3a8ef47bb9d7a4b9
John Vincent S. Guioguio <jevguio@gmail.com>
2025-09-30 20:38:46 +0800

    Fixes and Publish

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,13 +1,14 @@
 import black from "../../img/black-transparent (1).png";
 import white from "../../img/for-dark-background.png";
+import anjo from "../../img/anjo-logo.png";
 export default function ApplicationLogo(props) {
     return (
         <div {...props}>
             {/* Light mode logo */}
-            <img src={black} alt="Logo" className="block dark:hidden h-10" />
+            <img src={anjo} alt="Logo" className="block dark:hidden h-10" />
 
             {/* Dark mode logo */}
-            <img src={white} alt="Logo" className="hidden dark:block h-10" />
+            <img src={anjo} alt="Logo" className="hidden dark:block h-10" />
         </div>
     );
 }
